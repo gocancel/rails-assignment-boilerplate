679d96c4a206d41b44249090a8b88128faa93a2a
oborze <borzemska@gmail.com>
2022-04-19 02:05:50 +0200

    Feature tests - error path

--- spec/features/books_management_spec.rb
+++ spec/features/books_management_spec.rb
@@ -28,4 +28,45 @@ RSpec.feature 'Books creation', type: :feature do
     expect(page).to have_link(nil, href: book_path(created_book))
     expect(page).to have_css("img[src='#{rails_blob_path(created_book.cover)}']", count: 1)
   end
+
+  scenario 'User tries to create empty author', js: true do
+    visit books_path
+
+    click_link 'Add a new book'
+
+    expect(page).to have_text('Author missing? Add an author')
+    click_link 'Author missing? Add an author'
+
+    fill_in 'Name of the author', with: nil
+    click_button 'Create Author'
+
+    expect(page).to have_text("Name can't be blank")
+
+    author_name = 'Cat specialist'
+    fill_in 'Name of the author', with: author_name
+    click_button 'Create Author'
+    expect(page).to have_text(author_name)
+  end
+
+  scenario 'User tries to create book with invalid parameters', js: true do
+    author = FactoryBot.create(:author)
+    visit books_path
+
+    click_link 'Add a new book'
+
+    click_button 'Create Book'
+
+    expect(page).to have_text("Title can't be blank")
+    expect(page).to have_text("Cover can't be blank")
+
+    fill_in 'Title', with: 'The great book of cats'
+    select 'Currently reading', from: 'book_shelf'
+    attach_file('book_cover', JPEG_PICTURE_PATH)
+    select author.name, from: 'book_author_id'
+    click_button 'Create Book'
+
+    created_book = Book.find_by(title: 'The great book of cats')
+    expect(page).to have_link(nil, href: book_path(created_book))
+    expect(page).to have_css("img[src='#{rails_blob_path(created_book.cover)}']", count: 1)
+  end
 end
