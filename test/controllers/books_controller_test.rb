d19e578879d70ddb64e951eb0130d84d316c8d5b
Oleg V <plotistlol@gmail.com>
2022-05-17 15:24:38 +0200

    Add books controller minitest

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,15 +2,27 @@ require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   test "book succesfully created" do
-    expect {
-      post books_path,
-           :book => {
-             title: "1984",
-             shelf: "read",
-             cover: File.open(Rails.root.join('/db/seeds/cover_1984.png')),
-             name: "George Orwell"
-           }
-    }.to change(Book, :count).by(1)
+
+    assert_difference 'Book.count' do
+      post :create, params: { book: {
+        title: "1984",
+        shelf: "read",
+        cover: File.open(Rails.root.join('/db/seeds/cover_1984.png')),
+        name: "George Orwell"
+      }}
+    end
+
+  end
+
+  test "failed to create book without title" do
+
+    assert_difference 'Book.count', 0 do
+      post :create, params: { book: {
+        shelf: "read",
+        cover: File.open(Rails.root.join('/db/seeds/cover_1984.png')),
+        name: "George Orwell"
+      }}
+    end
 
   end
 end
