fdaefd3b7fee22d8006cdf5cbe2322d36c5fd76f
Oleg V <plotistlol@gmail.com>
2022-05-17 12:46:57 +0200

    Add 'create' action and form; Add book data for generation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,28 @@ class BooksController < ApplicationController
   has_scope :by_shelf, as: :shelf
 
   def index
+    # TODO: Book.with_attached_avatar
     @books = apply_scopes(Book.includes(:cover_attachment)).all
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def	create
+    @book = Book.create!(book_params)
+    # @book.save ? redirect_to(book_path(@book)) : render(:new)
+
+    redirect_to @book
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :cover, :shelf)
   end
 end
